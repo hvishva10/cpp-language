7dae2b81a64b8c0108cf9dc477bde52fa15f33a5
vishva <hirparajyotsna@gmail.com>
2023-07-13 11:15:10 +0530

    cpp-language

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"hello world";
+    cout<<"\n enter a:";
+    cin>>a;
+    cout<<"\n enter b:";
+    cin>>b;
+    cout<<"product is :"<<a*b;
+}
